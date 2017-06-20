3a9f3cfa06272d3c0a91a5edde35f08215ab2c65
Paul Richard <Paul Richard>
2017-06-19 19:10:17 -0700

    added movie functionality

--- liri.js
+++ liri.js
@@ -20,10 +20,35 @@ var item = process.argv[3];
 // If else tree with commands:
 // Spotify:
 if (cmd === "spotify-this-song") {
-	spotify.search({ type: 'track', query: item }, function(err, data) {
-	  if (err) {
-	    return console.log('Error occurred: ' + err);
-	  }
-	console.log(data); 
+	spotify.search({ type: 'track', query: item }).then(function(response) {
+		console.log(response);
+		});
+}
+
+// function to console log movie info:
+function movieDisplay(body){
+	console.log(JSON.parse(body).Title);
+	console.log("Released: " + JSON.parse(body).Released);
+	console.log("IMDB Score: " + JSON.parse(body).imdbRating);
+	console.log("Country Produced In: " + JSON.parse(body).Country);
+	console.log("Original Language: " + JSON.parse(body).Language);
+	console.log("Plot Summary: " + JSON.parse(body).Plot);
+	console.log("Starring: " + JSON.parse(body).Actors);
+	console.log("Website: " + JSON.parse(body).Website);
+}
+
+// Dispalys Mr Nobody:
+if (cmd === "movie-this" && item != undefined) {
+	request("http://www.omdbapi.com/?t=" + item + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			movieDisplay(body);
+		}
+	});
+// If a movie is not entered displays Mr Nobody:
+} else if (cmd === "movie-this") {
+	request("http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			movieDisplay(body);
+		}
 	});
 }
